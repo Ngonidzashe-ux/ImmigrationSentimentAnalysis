7910fdbca9042e68993723ddaa0d1de86256e88a
Ngonidzashe-ux <ngonimaposss@gmail.com>
2024-01-16 05:02:12 +0800

    tweet.py file for scrapping tweets

--- tweet.py
+++ tweet.py
@@ -1,3 +1,52 @@
 #Scrapping tweets from twitter
 import tweepy
-from tweepy import OAuthHandler
+import requests
+import os
+import pandas as pd
+from dotenv import load_dotenv, find_dotenv
+
+#load the environment variables
+_ = load_dotenv(find_dotenv())
+
+api_key="9WAG5MiJzOPeYQ9h2LNlpmDKb"
+api_key_secret="SnSPu8CWoreRZwdCObzrUiBzZwA3fzYpBuSUIm9ilchxwWPQIi"
+access_token="1428601521718153218-7sAPkiKtBQBcXSUMlnRacZHEkQ9IKz"
+access_token_secret="7QrsYFUxyYZFhRixjffzOmGVoStzAF9FG1DGlzKXMgmVc"
+
+#retrieve credentials from environment variables
+# api_key = os.environ.get('X_API_KEY')
+# api_key_secret = os.environ.get('X_API_SECRET_KEY')
+# access_token = os.environ.get('X_ACCESS_TOKEN')
+# access_token_secret = os.environ.get('X_ACCESS_TOKEN_SECRET_SECRET')
+
+#pass the credentials in our twitter api to authenticate into your developer account
+auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
+
+#Instantiate our twitter api
+#This serves as a client that facilitates communication between our application and the twitter API servers
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+
+def get_user_tweets(username, count=5):
+    # Retrieve tweets from a specific user
+    tweets = api.user_timeline(screen_name=username, count=count, tweet_mode="extended")
+
+    # Pulling some attributes out of the tweets
+    attributes_container = [[tweet.user.name,  tweet.created_at, tweet.favorite_count, tweet.source, tweet.full_text] for tweet in tweets]
+    
+    # Creation of column list to rename the columns in the DataFrame
+    columns = ["User", "Created At", "Number of Likes", "Source of Tweet", "Tweet"]
+
+    # Creation of DataFrame
+    tweets_df = pd.DataFrame(attributes_container, columns=columns)
+    
+    return tweets_df
+
+# Example usage for Elon Musk's tweets
+try:
+    elon_tweets_df = get_user_tweets(username="elonmusk", count=5)
+    print(elon_tweets_df)
+
+except BaseException as e:
+    print(f"An unexpected error occurred: {e}")
